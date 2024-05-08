883459e4bd987b7fb35f836dd10391893979a02d
muse2023 <pandasuperfans@gmail.com>
2024-05-09 03:55:43 +0800

    增加postal api发送

--- app/Services/MailService.php
+++ app/Services/MailService.php
@@ -6,6 +6,8 @@ use App\Jobs\SendEmailJob;
 use App\Models\User;
 use App\Utils\CacheKey;
 use Illuminate\Support\Facades\Cache;
+use Postal\Client;
+use Postal\Send\Message;
 
 class MailService
 {
@@ -82,6 +84,7 @@ class MailService
      */
     public static function sendEmail(array $params)
     {
+        // 如果管理员设置了邮件相关配置，则更新 Laravel 配置
         if (admin_setting('email_host')) {
             \Config::set('mail.host', admin_setting('email_host', config('mail.host')));
             \Config::set('mail.port', admin_setting('email_port', config('mail.port')));
@@ -91,29 +94,50 @@ class MailService
             \Config::set('mail.from.address', admin_setting('email_from_address', config('mail.from.address')));
             \Config::set('mail.from.name', admin_setting('app_name', 'XBoard'));
         }
+
         $email = $params['email'];
         $subject = $params['subject'];
-        $params['template_name'] = 'mail.' . admin_setting('email_template', 'default') . '.' . $params['template_name'];
+        $templateName = 'mail.' . admin_setting('email_template', 'default') . '.' . $params['template_name'];
+        $templateValue = $params['template_value'];
+
+        $senderName = config('mail.from.name');
+        $senderAddress = config('mail.from.address');
+
         try {
-            \Mail::send(
-                $params['template_name'],
-                $params['template_value'],
-                function ($message) use ($email, $subject) {
-                    $message->to($email)->subject($subject);
-                }
-            );
+            $client = new \Postal\Client(config('mail.host'), config('mail.password'));
+            $message = new \Postal\Send\Message();
+
+            // 配置邮件发送信息
+            $message->to($email);
+            $message->from("$senderName <$senderAddress>");
+            $message->sender($senderAddress);
+            $message->subject($subject);
+
+            // 渲染邮件模板并设置为邮件内容
+            $htmlBody = view($templateName, $templateValue)->render();
+            $message->htmlBody($htmlBody);
+
+            // 使用 Postal 客户端发送邮件
+            $client->send->message($message);
+
             $error = null;
         } catch (\Exception $e) {
+            // 捕获异常并记录错误信息
             $error = $e->getMessage();
         }
+
+        // 记录邮件发送日志
         $log = [
-            'email' => $params['email'],
-            'subject' => $params['subject'],
-            'template_name' => $params['template_name'],
+            'email' => $email,
+            'subject' => $subject,
+            'template_name' => $templateName,
             'error' => $error,
             'config' => config('mail')
         ];
+
         \App\Models\MailLog::create($log);
+
         return $log;
     }
+
 }
