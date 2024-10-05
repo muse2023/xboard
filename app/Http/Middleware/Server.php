51ca8e2578428c2d52c405aad463a87fed796027
muse2023 <pandasuperfans@gmail.com>
2024-10-06 01:28:34 +0800

    修复重大安全漏洞 token未做空验证,可暴力破解

--- app/Http/Middleware/Server.php
+++ app/Http/Middleware/Server.php
@@ -18,6 +18,11 @@ class Server
      */
     public function handle(Request $request, Closure $next, $node_type = null)
     {
+        $token = $request->input('token');
+        if (empty($token)) {
+            throw new ApiException('token is null', 403);
+        }
+
         // alias
         $aliasTypes = [
             'v2ray' => 'vmess',
@@ -37,7 +42,7 @@ class Server
                 'nullable',
                 'regex:/^(?i)(hysteria|hysteria2|vless|trojan|vmess|v2ray|tuic|shadowsocks|shadowsocks-plugin)$/',
                 function ($attribute, $value, $fail) use ($aliasTypes, $request) {
-                    $request->merge([$attribute => strtolower(isset ($aliasTypes[$value]) ? $aliasTypes[$value] : $value)]);
+                    $request->merge([$attribute => strtolower(isset($aliasTypes[$value]) ? $aliasTypes[$value] : $value)]);
                 },
             ]
         ], [
