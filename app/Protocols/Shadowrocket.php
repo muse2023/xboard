e75784d01822f42736500eb8d1cb83602f4d454e
muse2023 <pandasuperfans@gmail.com>
2024-10-18 16:46:17 +0800

    修改小火箭头部

--- app/Protocols/Shadowrocket.php
+++ app/Protocols/Shadowrocket.php
@@ -27,11 +27,7 @@ class Shadowrocket
         $upload = round($user['u'] / (1024*1024*1024), 2);
         $download = round($user['d'] / (1024*1024*1024), 2);
         $totalTraffic = round($user['transfer_enable'] / (1024*1024*1024), 2);
-        if (empty($user['expired_at'])) {
-            $expiredDate = '不限时';
-        } else {
-            $expiredDate = date('Y-m-d', $user['expired_at']);
-        }
+        $expiredDate = date('Y-m-d', $user['expired_at']);
         $uri .= "STATUS=🚀↑:{$upload}GB,↓:{$download}GB,TOT:{$totalTraffic}GB💡Expires:{$expiredDate}\r\n";
         foreach ($servers as $item) {
             if ($item['type'] === 'shadowsocks') {
