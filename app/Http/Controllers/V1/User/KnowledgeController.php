6f58472240e58d62a7bb8230488c096b09673a86
muse2023 <pandasuperfans@gmail.com>
2024-10-18 18:32:01 +0800

    知识库+小火箭下发修改

--- app/Http/Controllers/V1/User/KnowledgeController.php
+++ app/Http/Controllers/V1/User/KnowledgeController.php
@@ -26,7 +26,7 @@ class KnowledgeController extends Controller
                 $this->formatAccessData($knowledge['body']);
             }
             $subscribeUrl = Helper::getSubscribeUrl("/api/v1/client/subscribe?token={$user['token']}");
-            $subscribeEmbyUrl = preg_replace('/https?:\/\/[^\/]+/', "https://verifylook.bigme.pro", $subscribeUrl);
+            $subscribeEmbyUrl = "https://verifylook.bigme.pro" . "/api/v1/client/subscribe?token={$token}";
             $knowledge['body'] = str_replace('{{siteName}}', admin_setting('app_name', 'XBoard'), $knowledge['body']);
             $knowledge['body'] = str_replace('{{subscribeUrl}}', $subscribeUrl, $knowledge['body']);
             $knowledge['body'] = str_replace('{{subscribeEmbyUrl}}', $subscribeEmbyUrl, $knowledge['body']);
